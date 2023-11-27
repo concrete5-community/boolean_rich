db57534ce060aa04979f97e7440a6918d68c1cf9
Michele Locati <michele@locati.it>
2023-11-27 09:58:39 +0100

    Version 1.1.0

--- controller.php
+++ controller.php
@@ -15,7 +15,7 @@ class Controller extends Package implements ProviderAggregateInterface
 {
     protected $pkgHandle = 'boolean_rich';
 
-    protected $pkgVersion = '1.0.0';
+    protected $pkgVersion = '1.1.0';
 
     /**
      * {@inheritdoc}
